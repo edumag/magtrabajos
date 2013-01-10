5ec7054a7f46710d966fa905d67f537dd091ec45
edumag <eduardomagrane@gmail.com>
2013-01-10 12:36:39 +0100

    Añadimos barra de información de magcontrolversion al menu principal

---
+++ plugins/magcontrolversion/eventos/menu_principal/estado_control_versiones.sh
@@ -0,0 +1,13 @@
+## @file estado_control_versiones
+## @brief Presentar estado de control de versiones
+
+# Si tenim fitxes bloquejats els presentem
+if [ "`plugin_activado magcontrolversion`" != "" ] ; then 
+   local total_cambios="$(magcontrolversion status | wc -l | cut -d' ' -f1)"
+   local rama_actual="$(magcontrolversion rama_actual)"
+   SALIDA="Rama actual: $rama_actual / Archivos con cambios $total_cambios"
+   if [ "$SALIDA" != "" ] ; then
+      caja "$SALIDA" 'Control de versiones' $colAviso 0
+   fi
+fi
+
