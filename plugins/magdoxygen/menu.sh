aac4c9f706e416d8ef61190bc7448e9915ebba3b
edumag <eduardomagrane@gmail.com>
2013-11-18 14:00:26 +0100

    Tenemos en cuenta descartados al sincronizar servidor

--- plugins/magdoxygen/menu.sh
+++ plugins/magdoxygen/menu.sh
@@ -7,16 +7,16 @@ if [ "$movimientoId" != "" ] ; then
 
 SQL="$SQL_ARC AND idMovimiento=$movimientoId"
 
-   magdoxygen conf "$HOME/.magtrabajos/proyectos/$proyecto/doxygen.conf" menu $(ejecutarSQL "$SQL" "-N")
+   magdoxygen conf ".magtrabajos/doxygen.conf" menu $(ejecutarSQL "$SQL" "-N")
 
 elif [ "$tareaId" != "" ] ; then
 
 local SQL="SELECT DISTINCT r.nombre FROM tareas t, movimientos m, rMovimientoArchivos r WHERE m.idTarea=t.id AND m.id=r.idMovimiento AND t.id=$tareaId ORDER BY r.nombre"
 
-   magdoxygen conf "$HOME/.magtrabajos/proyectos/$proyecto/doxygen.conf" menu $(ejecutarSQL "$SQL" "-N")
+   magdoxygen conf ".magtrabajos/doxygen.conf" menu $(ejecutarSQL "$SQL" "-N")
 
 else
 
-   magdoxygen conf "$HOME/.magtrabajos/proyectos/$proyecto/doxygen.conf" menu
+   magdoxygen conf ".magtrabajos/doxygen.conf" menu
 
 fi
